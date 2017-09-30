3d82d2692eba664b068d4ed33b2d16e96d0f734c
Marcelo Grossi <magrossi@gmail.com>
2017-09-30 11:36:34 +0100

    Update ElasticSearchRestAppender.java

--- src/main/java/com/github/magrossi/log4j2/elasticsearch/ElasticSearchRestAppender.java
+++ src/main/java/com/github/magrossi/log4j2/elasticsearch/ElasticSearchRestAppender.java
@@ -325,7 +325,7 @@ public class ElasticSearchRestAppender extends AbstractAppender {
     
     @Override
     public void stop() {
-    	cancelTimer()
+    	cancelTimer();
     	this.send();
     	super.stop();
     }
